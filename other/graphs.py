939878f85d5cd3b0a6512b2989461e76d3cc1e4e
Mansur Ziiatdinov <gltronred@gmail.com>
2023-03-15 13:34:41 +0300

    Convert adjacency matrix to adjacency lists

--- other/graphs.py
+++ other/graphs.py
@@ -10,8 +10,15 @@ def edge_list_to_adj_matrix(v: int, edges: list[(int, int)]) -> list[list[bool]]
 
 def adj_matrix_to_adj_lists(a: list[list[bool]]) -> list[list[int]]:
     """Convert adjacency matrix to adjacency lists."""
-    pass
-
+    v = len(a)
+    r = []
+    for i in range(v):
+        ri = []
+        for j in range(v):
+            if a[i][j]:
+                ri.append(j)
+        r.append(ri)
+    return r
 
 if __name__ == '__main__':
     a = edge_list_to_adj_matrix(3, [(0,1), (1,2)])
