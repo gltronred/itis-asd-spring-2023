4c215c9b02df0559629084e49172cfffb6a9676a
Mansur Ziiatdinov <gltronred@gmail.com>
2023-03-15 12:30:31 +0300

    Convert list of edges to adjacency matrix

---
+++ other/graphs.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+def edge_list_to_adj_matrix(edges: list[(int, int)]) -> list[list[bool]]:
+    """Convert list of edges to adjacency matrix."""
+    pass
+
+if __name__ == '__main__':
+    print("Hello world!")
