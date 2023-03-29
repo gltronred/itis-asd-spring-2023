d41a76cb3312400e5e698b605e0579ff402c5c8a
Mansur Ziiatdinov <gltronred@gmail.com>
2023-03-29 14:42:29 +0300

    Permutations

--- bruteforce/gen.py
+++ bruteforce/gen.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
 
+import itertools
+
+
 def gen(n):
     """Collatz conjecture."""
     i = n
@@ -36,5 +39,55 @@ def powerset(set):
 #                            or S \ [1] is subset [2,...,9]
 
 
-for i in powerset([1, 2, 3]):
-    print(i)
+# for i in powerset([1, 2, 3]):
+#     print(i)
+
+
+# 1,2,3,4,5
+# 3,2,4,1,5
+
+# 1,2,3,4,5
+# 1,2,3,5,4
+# 1,2,4,3,5
+# ...
+# 5,4,3,2,1
+
+# 1,3,2,5,4
+#       ^ ^
+#       i
+#     2,5,4
+# 1,3,4,2,5
+# 1,3,4,5,2
+#       ^ ^
+#       i
+# 1,3,5,2,4
+
+def permutations(n):
+    """Return permutations of [1,2,...,n]."""
+    p = [i+1 for i in range(n)]
+
+    flag = True
+    while flag:
+        yield p
+
+        i = len(p) - 1
+        while i > 0 and p[i-1] > p[i]:
+            i -= 1
+        flag = i > 0
+        if flag:
+            # p[i-1] < p[i]
+            # find m: p[m] - minimal element greater than p[i-1]
+            m = i
+            for j in range(i+1, n):
+                if p[j] < p[m] and p[j] > p[i-1]:
+                    m = j
+            # swap p[m] and p[i-1]
+            t = p[m]
+            p[m] = p[i-1]
+            p[i-1] = t
+            # sort p[i:] - same as reverse p[i:] here
+            p[i:] = reversed(p[i:])
+
+
+for p in permutations(4):
+    print(p)
