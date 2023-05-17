0304ac4e2eb26e840d062dd99ab1dd90139edd05
Mansur Ziiatdinov <gltronred@gmail.com>
2023-05-17 12:53:20 +0300

    Compute angle

--- geometry/geom.py
+++ geometry/geom.py
@@ -1,9 +1,21 @@
 #!/usr/bin/env python3
 
+import math
+
+
 class Vector:
     def __init__(self, x: float, y: float):
         self.x = x
         self.y = y
+
+    def angle_bad(self):
+        if math.fabs(self.x) < 1e-6:
+            return math.pi / 2
+        else:
+            return math.atan(self.y / self.x)
+
+    def angle(self):
+        return math.atan2(self.y, self.x)
 
 
 class Point:
@@ -37,3 +49,15 @@ print(ccw(Vector(1, 1), Vector(2, 2)))  # collinear
 
 print(ccw_pt(Point(0, 0), Point(1, 1), Point(1, 2)))  # ccw
 print(ccw_pt(Point(0, 0), Point(1, 1), Point(1, 0)))  # cw
+
+p0 = Point(0, 0)
+p1 = Point(1, 1)    # angle = pi / 4
+p2 = Point(-1, -1)  # angle = -3 pi / 4
+
+v1 = p1.sub(p0)
+v2 = p2.sub(p0)
+
+print(v1.angle_bad())
+print(v2.angle_bad())
+print(v1.angle())
+print(v2.angle())
